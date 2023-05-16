03e1660f4940e43daecd4fc322b5a4265a3b2ded
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2023-05-16 19:06:18 +0200

    Add form validation for search mask and create layout for My Journeys page

--- src/components/SearchMask.tsx
+++ src/components/SearchMask.tsx
@@ -10,8 +10,11 @@ const SearchMask: React.FC<Props> = ({ onSearch }) => {
     const [destination, setDestination] = useState('');
     const [date, setDate] = useState('');
     const [time, setTime] = useState('');
+    const [formValid, setFormValid] = useState(false);
 
-    const handleSearchClick = () => {
+    const handleSearchClick = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
         const searchData = {
             departure,
             destination,
@@ -19,73 +22,82 @@ const SearchMask: React.FC<Props> = ({ onSearch }) => {
             time
         };
 
-        const searchResult = [{
-            departureTime: "10:16",
-            arrivalTime: "13:36",
-            meansOfTransport: ["EC, RJ, R"],
-            currentPrice: 64.90
-        }, {
-            departureTime: "11:18",
-            arrivalTime: "14:48",
-            meansOfTransport: ["RJ, R"],
-            currentPrice: 52.50
-        }]
-
+        const searchResult = [
+            {
+                departureTime: '10:16',
+                arrivalTime: '13:36',
+                meansOfTransport: ['EC, RJ, R'],
+                currentPrice: 64.90
+            },
+            {
+                departureTime: '11:18',
+                arrivalTime: '14:48',
+                meansOfTransport: ['RJ, R'],
+                currentPrice: 52.50
+            }
+        ];
 
         onSearch(searchData, searchResult);
     };
 
+    // Update form validity based on input fields
+    React.useEffect(() => {
+        setFormValid(departure.trim() !== '' && destination.trim() !== '' && date.trim() !== '' && time.trim() !== '');
+    }, [departure, destination, date, time]);
+
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={3} sm={1} container justifyContent="flex-end" alignItems="center">
-                <Typography>From:</Typography>
+        <form onSubmit={handleSearchClick}>
+            <Grid container spacing={2}>
+                <Grid item xs={3} sm={1} container justifyContent="flex-end" alignItems="center">
+                    <Typography>From:</Typography>
+                </Grid>
+                <Grid item xs={9} sm={5}>
+                    <TextField
+                        id="departure"
+                        value={departure}
+                        onChange={(e) => setDeparture(e.target.value)}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={3} sm={1} container justifyContent="flex-end" alignItems="center">
+                    <Typography>To:</Typography>
+                </Grid>
+                <Grid item xs={9} sm={5}>
+                    <TextField
+                        id="destination"
+                        value={destination}
+                        onChange={(e) => setDestination(e.target.value)}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={3} sm={1} container justifyContent="flex-end" alignItems="center">
+                    <Typography>Departure:</Typography>
+                </Grid>
+                <Grid item xs={5} sm={3}>
+                    <TextField
+                        id="date"
+                        type="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={4} sm={2}>
+                    <TextField
+                        id="time"
+                        type="time"
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item sm={6} container justifyContent="right" alignItems="center">
+                    <Button variant="contained" type="submit" disabled={!formValid}>
+                        Search
+                    </Button>
+                </Grid>
             </Grid>
-            <Grid item xs={9} sm={5}>
-                <TextField
-                    id="departure"
-                    value={departure}
-                    onChange={(e) => setDeparture(e.target.value)}
-                    fullWidth
-                />
-            </Grid>
-            <Grid item xs={3} sm={1} container justifyContent="flex-end" alignItems="center">
-                <Typography>To:</Typography>
-            </Grid>
-            <Grid item xs={9} sm={5}>
-                <TextField
-                    id="destination"
-                    value={destination}
-                    onChange={(e) => setDestination(e.target.value)}
-                    fullWidth
-                />
-            </Grid>
-            <Grid item xs={3} sm={1} container justifyContent="flex-end" alignItems="center">
-                <Typography>Departure:</Typography>
-            </Grid>
-            <Grid item xs={5} sm={3}>
-                <TextField
-                    id="date"
-                    type="date"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                    fullWidth
-                />
-            </Grid>
-            <Grid item xs={4} sm={2}>
-                <TextField
-                    id="time"
-                    type="time"
-                    value={time}
-                    onChange={(e) => setTime(e.target.value)}
-                    fullWidth
-                />
-            </Grid>
-            <Grid item sm={6} container justifyContent="right" alignItems="center">
-                <Button variant="contained" onClick={handleSearchClick}>
-                    Search
-                </Button>
-            </Grid>
-        </Grid>
+        </form>
     );
 };
 
