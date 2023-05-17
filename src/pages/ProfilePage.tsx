bceeb79402e374f601c3b707f831997097dfb9e2
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2023-05-17 10:57:25 +0200

    Add profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,14 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Typography, Grid, TextField, Button, Avatar, Box } from '@mui/material';
 
-interface Props {}
+const ProfilePage: React.FC = () => {
+  const [firstName, setFirstName] = useState('John');
+  const [lastName, setLastName] = useState('Doe');
+  const [email, setEmail] = useState('johndoe@example.com');
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
 
-const ProfilePage: React.FC<Props> = () => {
+  const handleProfilePictureChange = () => {
+    // Logic to handle picture change
+  };
+
+  const handleSaveChanges = () => {
+    // Handle save changes logic
+  };
+
   return (
-    <div>
-      {/* your component content goes here */}
-    </div>
+    <Container maxWidth="md" sx={{ my: 4 }}>
+      <Typography variant="h6" gutterBottom>
+        Profile
+      </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'left', alignItems: 'center', marginBottom: 4 }}>
+        <Avatar alt="Profile Picture" src="/path/to/profile-picture.jpg" sx={{ width: 120, height: 120 }} />
+        <Box sx={{ flexGrow: 0, marginLeft: 2 }}>
+          <Button variant="outlined" component="label" >
+            Change Picture
+            <input type="file" accept="image/*" hidden onChange={handleProfilePictureChange} />
+          </Button>
+        </Box>
+      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField label="First Name" variant="outlined" value={firstName} onChange={(e) => setFirstName(e.target.value)} fullWidth disabled />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField label="Last Name" variant="outlined" value={lastName} onChange={(e) => setLastName(e.target.value)} fullWidth disabled />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} fullWidth disabled />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id="oldPassword"
+            label="Old Password"
+            type="password"
+            value={oldPassword}
+            onChange={(e) => setOldPassword(e.target.value)}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id="newPassword"
+            label="New Password"
+            type="password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" color="primary" onClick={handleSaveChanges}>
+            Save Changes
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
 export default ProfilePage;
-
