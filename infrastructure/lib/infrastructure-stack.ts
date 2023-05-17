16c1a609f556c273f039c0714dd060362c710ca5
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2023-05-17 16:34:53 +0200

    Add Cognito authentication construct

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CognitoAuth } from './cognito-auth';
+
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CognitoAuth(this, 'CognitoAuth')
   }
 }
