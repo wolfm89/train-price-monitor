0bdd3cdf6c6bca7a1a3eced9f8e28c9481b7d06f
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2023-05-19 17:33:00 +0200

    Add Cognito signup

--- frontend/src/components/SignupModal.tsx
+++ frontend/src/components/SignupModal.tsx
@@ -1,5 +1,6 @@
 import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from '@mui/material';
 import React, { useState } from 'react';
+import { CognitoUserPool, CognitoUserAttribute, ISignUpResult } from 'amazon-cognito-identity-js';
 
 interface Props {
   open: boolean;
@@ -8,7 +9,16 @@ interface Props {
 
 const SignupModal: React.FC<Props> = ({ open, onClose }) => {
   const [email, setEmail] = useState<string>('');
+  const [givenName, setGivenName] = useState<string>('');
+  const [familyName, setFamilyName] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+
+  const poolData = {
+    UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID!,
+    ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID!,
+  };
+
+  const userPool = new CognitoUserPool(poolData);
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -18,16 +28,53 @@ const SignupModal: React.FC<Props> = ({ open, onClose }) => {
     setPassword(e.target.value);
   };
 
+  const handleGivenNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGivenName(e.target.value);
+  };
+
+  const handleFamilyNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFamilyName(e.target.value);
+  };
+
   const handleSignUp = () => {
-    // Add your sign-up logic here
+    const userAttributes: CognitoUserAttribute[] = [
+      new CognitoUserAttribute({ Name: 'email', Value: email }),
+      new CognitoUserAttribute({ Name: 'given_name', Value: givenName }),
+      new CognitoUserAttribute({ Name: 'family_name', Value: familyName }),
+    ];
+
+    userPool.signUp(email, password, userAttributes, [], (err: Error | undefined, result?: ISignUpResult) => {
+      if (err) {
+        console.error('Error signing up:', err);
+        return;
+      }
+      console.log('Successfully signed up:', result);
+    });
   };
 
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Sign Up</DialogTitle>
       <DialogContent>
-        <TextField autoFocus margin="dense" id="firstName" label="First Name" type="text" fullWidth />
-        <TextField margin="dense" id="lastName" label="Last Name" type="text" fullWidth />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="firstName"
+          label="First Name"
+          type="text"
+          fullWidth
+          value={givenName}
+          onChange={handleGivenNameChange}
+        />
+        <TextField
+          margin="dense"
+          id="lastName"
+          label="Last Name"
+          type="text"
+          fullWidth
+          value={familyName}
+          onChange={handleFamilyNameChange}
+        />
         <TextField margin="dense" label="Email" type="email" fullWidth value={email} onChange={handleEmailChange} />
         <TextField
           margin="dense"
