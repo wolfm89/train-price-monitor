6a3105fa38b91d89999defbe7365a1219194c7ef
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2023-06-13 14:08:07 +0200

    Improve docker setup and add React variables to Github workflow (#7)

--- backend/src/schema/generated/resolvers.generated.ts
+++ backend/src/schema/generated/resolvers.generated.ts
@@ -1,5 +1,11 @@
-import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
-export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+import {
+  GraphQLResolveInfo,
+  GraphQLScalarType,
+  GraphQLScalarTypeConfig,
+} from "graphql";
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]-?: NonNullable<T[P]>;
+};
 
 export type ResolverTypeWrapper<T> = Promise<T> | T;
 
@@ -31,9 +37,25 @@ export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
-export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
-  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+export interface SubscriptionSubscriberObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> {
+  subscribe: SubscriptionSubscribeFn<
+    { [key in TKey]: TResult },
+    TParent,
+    TContext,
+    TArgs
+  >;
+  resolve?: SubscriptionResolveFn<
+    TResult,
+    { [key in TKey]: TResult },
+    TContext,
+    TArgs
+  >;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
@@ -41,12 +63,26 @@ export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
   resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
-export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+export type SubscriptionObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> =
   | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
   | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
-export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
-  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+export type SubscriptionResolver<
+  TResult,
+  TKey extends string,
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> =
+  | ((
+      ...args: any[]
+    ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
   | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
@@ -63,7 +99,12 @@ export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
 
 export type NextResolverFn<T> = () => Promise<T>;
 
-export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+export type DirectiveResolverFn<
+  TResult = {},
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> = (
   next: NextResolverFn<TResult>,
   parent: TParent,
   args: TArgs,
@@ -73,88 +114,101 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
-  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
-  File: ResolverTypeWrapper<Scalars['File']>;
-  ID: ResolverTypeWrapper<Scalars['ID']>;
+  Boolean: ResolverTypeWrapper<Scalars["Boolean"]>;
+  File: ResolverTypeWrapper<Scalars["File"]>;
+  ID: ResolverTypeWrapper<Scalars["ID"]>;
   Mutation: ResolverTypeWrapper<{}>;
   PresignedUrl: ResolverTypeWrapper<PresignedUrl>;
   Query: ResolverTypeWrapper<{}>;
-  String: ResolverTypeWrapper<Scalars['String']>;
+  String: ResolverTypeWrapper<Scalars["String"]>;
   User: ResolverTypeWrapper<User>;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
-  Boolean: Scalars['Boolean'];
-  File: Scalars['File'];
-  ID: Scalars['ID'];
+  Boolean: Scalars["Boolean"];
+  File: Scalars["File"];
+  ID: Scalars["ID"];
   Mutation: {};
   PresignedUrl: PresignedUrl;
   Query: {};
-  String: Scalars['String'];
+  String: Scalars["String"];
   User: User;
 };
 
-export interface FileScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['File'], any> {
-  name: 'File';
+export interface FileScalarConfig
+  extends GraphQLScalarTypeConfig<ResolversTypes["File"], any> {
+  name: "File";
 }
 
 export type MutationResolvers<
   ContextType = any,
-  ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']
+  ParentType extends ResolversParentTypes["Mutation"] = ResolversParentTypes["Mutation"]
 > = {
   activateUser?: Resolver<
-    Maybe<ResolversTypes['User']>,
+    Maybe<ResolversTypes["User"]>,
     ParentType,
     ContextType,
-    RequireFields<MutationActivateUserArgs, 'id'>
+    RequireFields<MutationActivateUserArgs, "id">
   >;
   createUser?: Resolver<
-    Maybe<ResolversTypes['User']>,
+    Maybe<ResolversTypes["User"]>,
     ParentType,
     ContextType,
-    RequireFields<MutationCreateUserArgs, 'email' | 'familyName' | 'givenName' | 'id'>
+    RequireFields<
+      MutationCreateUserArgs,
+      "email" | "familyName" | "givenName" | "id"
+    >
   >;
   updateUserProfilePicture?: Resolver<
-    Maybe<ResolversTypes['User']>,
+    Maybe<ResolversTypes["User"]>,
     ParentType,
     ContextType,
-    RequireFields<MutationUpdateUserProfilePictureArgs, 'id' | 'image'>
+    RequireFields<MutationUpdateUserProfilePictureArgs, "id" | "image">
   >;
 };
 
 export type PresignedUrlResolvers<
   ContextType = any,
-  ParentType extends ResolversParentTypes['PresignedUrl'] = ResolversParentTypes['PresignedUrl']
+  ParentType extends ResolversParentTypes["PresignedUrl"] = ResolversParentTypes["PresignedUrl"]
 > = {
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  url?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  url?: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
 export type QueryResolvers<
   ContextType = any,
-  ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']
+  ParentType extends ResolversParentTypes["Query"] = ResolversParentTypes["Query"]
 > = {
-  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'id'>>;
-  userProfilePicturePresignedUrl?: Resolver<
-    Maybe<ResolversTypes['PresignedUrl']>,
+  user?: Resolver<
+    Maybe<ResolversTypes["User"]>,
     ParentType,
     ContextType,
-    RequireFields<QueryUserProfilePicturePresignedUrlArgs, 'id'>
+    RequireFields<QueryUserArgs, "id">
+  >;
+  userProfilePicturePresignedUrl?: Resolver<
+    Maybe<ResolversTypes["PresignedUrl"]>,
+    ParentType,
+    ContextType,
+    RequireFields<QueryUserProfilePicturePresignedUrlArgs, "id">
   >;
 };
 
 export type UserResolvers<
   ContextType = any,
-  ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']
+  ParentType extends ResolversParentTypes["User"] = ResolversParentTypes["User"]
 > = {
-  activated?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
-  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  familyName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  givenName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  profilePicture?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  activated?: Resolver<ResolversTypes["Boolean"], ParentType, ContextType>;
+  email?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  familyName?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  givenName?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  profilePicture?: Resolver<
+    Maybe<ResolversTypes["String"]>,
+    ParentType,
+    ContextType
+  >;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
