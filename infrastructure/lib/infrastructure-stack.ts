f28d9159c7a241ca4ab0056fecb619dee22770bb
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2023-11-27 20:38:20 +0100

    Add ToolkitCleaner to keep CDK assets clean

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Schedule } from 'aws-cdk-lib/aws-events';
+import { ToolkitCleaner } from 'cloudstructs/lib/toolkit-cleaner';
 import { CognitoAuth } from './cognito-auth';
 import { Backend } from './backend';
 import { Frontend } from './frontend';
@@ -11,5 +13,8 @@ export class InfrastructureStack extends cdk.Stack {
     const cognitoAuth = new CognitoAuth(this, 'CognitoAuth');
     new Backend(this, 'Backend', cognitoAuth.userPool);
     new Frontend(this, 'Frontend');
+    new ToolkitCleaner(this, 'ToolkitCleaner', {
+      schedule: Schedule.rate(cdk.Duration.days(7)),
+    });
   }
 }
